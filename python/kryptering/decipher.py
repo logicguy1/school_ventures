a6c8f10e455befac6033e156d4a001bf32f10a1e
sleepy <78026100+sleepyri@users.noreply.github.com>
2022-09-08 17:25:51 +0200

    Delete decipher.py

--- python/kryptering/decipher.py
+++
@@ -1,8 +0,0 @@
-file = "txt/chiffer.txt"
-
-with open(file, encoding="utf8") as f:
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        print(line.strip())
