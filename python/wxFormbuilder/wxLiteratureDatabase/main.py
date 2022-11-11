7bee515a84475385cea63d5b5903ca5af0662f9b
sleepy <78026100+sleepyri@users.noreply.github.com>
2022-11-11 22:39:39 +0100

    Update main.py

--- python/wxFormbuilder/wxLiteratureDatabase/main.py
+++ python/wxFormbuilder/wxLiteratureDatabase/main.py
@@ -1,30 +1,7 @@
 import wx
 import gui
 import sqlite3 as lite
-
-
-class DatabaseModel():
-    #def setParameters():
-
-    def getBooks(self, con):
-        sql = """SELECT id, title, surname, firstname, publisher, year, genre FROM books"""
-        cur = con.execute(sql)
-        all_books = cur.fetchall()
-        return all_books
-    
-    def getArticle(self, con):
-        sql = """SELECT id, title, surname, firstname, magazine, issue, date FROM"""
-        cur = con.execute(sql)
-        all_articles = cur.fetchall()
-        return all_articles
-
-    def addBook(self, con):
-        sql = """INSERT INTO books (title, surname, firstname, publisher, year, genre) values (?, ?, ?, ?, ?, ?')"""
-        cur = con.execute(sql)
-        dataAdd = cur.fetchall()
-        return dataAdd
-
-    #def insertBook():
+from database_model import DatabaseModel
 
 class MainFrame(gui.MainFrame):
     """
@@ -33,10 +10,9 @@ class MainFrame(gui.MainFrame):
     Methods:
         btn_search = Searches database
         onClose = exits window
-
     """
 
-    def __init__(self, parent):
+    def __init__(self, parent): 
         gui.MainFrame.__init__(self, parent)
         self.LogFrame = LogFrame(self)
 
@@ -66,41 +42,55 @@ class MainFrame(gui.MainFrame):
             genre = "%"
         
         # Make query statement
-        sql_query = """SELECT title, surname, firstname, publisher, year, genre FROM books WHERE title LIKE ? AND surname LIKE ? AND firstname LIKE ? AND publisher LIKE ? AND year LIKE ? AND genre LIKE ? """
+        sql_query = """SELECT title, surname, firstname, publisher, year, genre FROM books WHERE title LIKE ? AND 
+                    surname LIKE ? AND firstname LIKE ? AND publisher LIKE ? AND year LIKE ? AND genre LIKE ? """
         search_args = (title, sname, fname, publisher, year, genre)
         cursor = self.con.execute(sql_query, search_args)
         book_data = cursor.fetchall()
         self.dataView_book.DeleteAllItems()
 
-        for item in book_data:
-            self.dataView_book.AppendItem(item)
+        for book in book_data:
+            self.dataView_book.AppendItem(book)
 
     def loadData(self, event):
         self.dataView_viewDatabase.DeleteAllItems()
-        data = self.dataModel.getBooks(self.con)
+        self.dataView_viewArticles.DeleteAllItems()
+        article_data = self.dataModel.getArticle(self.con)
+        book_data = self.dataModel.getBooks(self.con)
+        
+        for item in article_data:
+            itemView = (item[1],item[2],item[3],item[4],item[5],item[6])
+            self.dataView_viewArticles.AppendItem(itemView)
 
-        for item in data:
-            self.dataView_viewDatabase.AppendItem(item)
+        for item in book_data:
+            itemView = (item[1],item[2],item[3],item[4],item[5],item[6])
+            self.dataView_viewDatabase.AppendItem(itemView)
 
     def btn_log(self, event):
         self.LogFrame.Show(True)
 
     def btn_add(self, event):
-        
+
         dlg = gui.EditDialog(self)
         result = dlg.ShowModal()
 
         if result == wx.ID_OK:
+
             fname = dlg.dlg_textCtrl_fname.GetValue()
             sname = dlg.dlg_textCtrl_sname.GetValue()
             title = dlg.dlg_textCtrl_title.GetValue()
-            publisher = dlg.dlg_textCtrl_publish.GetValue()
+            publisher = dlg.dlg_textCtrl_publish.GetValue() 
             year = dlg.dlg_textCtrl_year.GetValue()
             genre = dlg.dlg_choice_genre.GetStringSelection()
-            
-            print("Button clicked")
-            self.con.addBook(fname, sname, title, publisher, year, genre)
+                        
+            self.dataModel.addBook(self.con, (title, sname, fname, publisher, year, genre))
             self.staticText_alert.SetLabel("New book has been added")
+
+        if result == wx.ID_CLOSE:
+            exit()
+
+        if result == wx.ID_DELETE:
+            self.dataModel.deleteItems(self.con)
 
     def onClose(self, event):
         self.con.close()
@@ -110,9 +100,6 @@ class LogFrame(gui.LogFrame):
 
     def __init__(self, parent):
         gui.LogFrame.__init__(self, parent)
-
-    def onClose(self, event):
-        exit()
 
 app = wx.App(False)
 frame = MainFrame(None)
