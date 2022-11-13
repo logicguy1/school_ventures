c4858fd4a1154d2372fd75613d4307059e818b9b
sleepy <78026100+sleepyri@users.noreply.github.com>
2022-11-13 19:28:27 +0100

    Update database_model.py

--- python/wxFormbuilder/wxLiteratureDatabase/database_model.py
+++ python/wxFormbuilder/wxLiteratureDatabase/database_model.py
@@ -1,5 +1,23 @@
 class DatabaseModel():
 
+    def __init__(self, pubsub):
+        self.pb = pubsub
+
+    def active_log(func):
+        def wrapper(self, *args, **kwargs):
+            self.pb.sendMessage("MyMainFrame", message = f"{func.__name__} - {args} - {func.__doc__}")
+            return func(self, *args, **kwargs)
+        return wrapper
+
+    @active_log
+    def searchBooks(self, con, args):
+        sql = """SELECT title, surname, firstname, publisher, year, genre FROM books WHERE title LIKE ? AND 
+                surname LIKE ? AND firstname LIKE ? AND publisher LIKE ? AND year LIKE ? AND genre LIKE ? """
+        cur = con.execute(sql, args)
+        book_data = cur.fetchall()
+        return book_data
+
+    @active_log
     def getBooks(self, con):
         """Get books from given input"""
         sql = """SELECT id, title, surname, firstname, publisher, year, genre FROM books"""
@@ -7,6 +25,7 @@ class DatabaseModel():
         all_books = cur.fetchall()
         return all_books
     
+    @active_log
     def getArticle(self, con):
         """Get articles from given input"""
         sql = """SELECT id, title, surname, firstname, magazine, issue, date FROM articles"""
@@ -14,14 +33,30 @@ class DatabaseModel():
         all_articles = cur.fetchall()
         return all_articles
 
+    @active_log
     def addBook(self, con, tuple):
-        """Executes sql insert statement and commits it to database"""
+        """Executes sql insert statement and commits it to book database"""
         sql = """INSERT INTO books (title, surname, firstname, publisher, year, genre) values (?, ?, ?, ?, ?, ?)"""
         cur = con.execute(sql, tuple)
         con.commit()
 
-    def deleteItems(self, con):
-        sql = """DELETE FROM books (title, surname, firstname, publisher, year, genre) WHERE (?, ?, ?, ?, ?, ?)"""
-        cur = con.execute(sql)
-        delete_book = cur.fetchall()
-        return delete_book
+    @active_log
+    def addArticle(self, con, tuple):
+        """Executes sql insert statement and commits it to article database"""
+        sql = """INSERT INTO articles (title, surname, firstname, magazine, issue, date) values (?, ?, ?, ?, ?, ?)"""
+        cur = con.execute(sql, tuple)
+        con.commit()
+
+    @active_log
+    def deleteBook(self, con, id):
+        sql = """DELETE FROM books WHERE id = ?"""
+        tuple = (id,)
+        cur = con.execute(sql, tuple)
+        con.commit()
+
+    @active_log
+    def deleteArticle(self, con, id):
+        sql = """DELETE FROM articles WHERE id = ?"""
+        tuple = (id,)
+        cur = con.execute(sql, tuple)
+        con.commit()
